d07bbe2296ff2f8ea5fe04ee48d04b1bceb1e6e9
adityakarn <adityakarn26@gmail.com>
2020-03-29 17:52:31 +0530

    added sidebar

--- src/index.js
+++ src/index.js
@@ -28,7 +28,13 @@ io.on('connection', (socket) => {
         socket.join(user.room)
 
         socket.emit('message', generateMessage('Admin','Welcome!'))
+        
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`))
+       
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
